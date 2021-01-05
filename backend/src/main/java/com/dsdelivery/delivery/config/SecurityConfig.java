2c82b70d26c50dc4d9fe1f96e70ee479bb90568b
Pedro Henrique <pdr_castro@hotmail.com>
2021-01-04 21:26:49 -0300

    domain model

--- backend/src/main/java/com/dsdelivery/delivery/config/SecurityConfig.java
+++ backend/src/main/java/com/dsdelivery/delivery/config/SecurityConfig.java
@@ -40,4 +40,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
+	
 }
---
+++ backend/src/main/java/com/dsdelivery/delivery/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.dsdelivery.delivery.entities;
+
+public enum OrderStatus {
+	PENDING, DELIVERED;
+}
