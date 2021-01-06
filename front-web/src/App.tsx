7b5f3e0df60261a32b5fcde38c7a2e54156ab929
Pedro Henrique <pdr_castro@hotmail.com>
2021-01-06 08:53:52 -0300

    create navbar component and define global css

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ front-web/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+import  logo  from './../../assets/images/logo.svg'
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <img className = "logo" src={logo} alt="logo"></img>
+            <a href="/">Delivery</a>
+        </nav>
+    )
+}
+
+export default Navbar;
