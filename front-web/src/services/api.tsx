c9fa60b2ce25f5e521d92b8099d8b52de4d17cd4
Pedro Henrique <pdr_castro@hotmail.com>
2021-01-09 00:24:26 -0300

    fix env variable and add redirect file

--- front-web/src/services/api.tsx
+++ front-web/src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://delivery-sds.herokuapp.com'
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
     baseURL: API_URL,
--- front-web/src/services/mapApi.tsx
+++ front-web/src/services/mapApi.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const mapboxToken = process.env.REACT_APP_ACESS_URL;
+const mapboxToken = process.env.REACT_APP_ACCESS_URL;
 
 export default function fetchLocalMapBox(local: string) {
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
